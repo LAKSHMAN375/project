669a23abefcdebaa02f7bfc3939eecda18c7fc18
LAKSHMAN375 <125733859+LAKSHMAN375@users.noreply.github.com>
2023-09-17 15:27:03 +0530

    login2.py

--- login2.py
+++ login2.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-# Define your other Streamlit app's file URL here
-other_app_file_url = "https://smsspamdetectionusingnlp.streamlit.app/#smishing-sms-phishing"
-
 st.title('Login and Signup')
 
+show_login = st.button('Log In')
+show_signup = st.button('Sign Up')
+
 # User registration form
-if st.button('Sign Up'):
+if show_signup:
     st.subheader('User Registration')
     new_username = st.text_input('Enter a new username:')
     new_password = st.text_input('Enter a new password:', type='password')
@@ -18,7 +18,7 @@ if st.button('Sign Up'):
             st.warning('Please enter both a username and password.')
 
 # User login form
-if st.button('Log In'):
+if show_login:
     st.subheader('User Login')
     username = st.text_input('Enter your username:')
     password = st.text_input('Enter your password:', type='password')
@@ -27,8 +27,9 @@ if st.button('Log In'):
             # Your login logic here
             if username == "your_username" and password == "your_password":
                 st.success('Login successful.')
-                # Add a link to the other app or file
-                st.markdown(f"[Click here to access the other app]({other_app_file_url})")
+                
+other_app_file_url = "https://smsspamdetectionusingnlp.streamlit.app/#smishing-sms-phishing"
+ st.markdown(f"[Click here to access the other app]({other_app_file_url})")
             else:
                 st.warning('Invalid credentials. Please try again.')
         else:
