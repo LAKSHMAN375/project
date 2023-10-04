6130340b8e5dec96ccde6b4d3f96decc473e0f7f
LAKSHMAN375 <125733859+LAKSHMAN375@users.noreply.github.com>
2023-10-04 19:40:31 +0530

    app.py

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 # Load your dataset
+'''
 data = pd.read_excel("SMSSpamCollection.xlsx", header=None)
 data.rename(columns={0: 'Category', 1: 'Email Text'}, inplace=True)
-last_10_messages = data.tail(22)['Email Text'].tolist()
+last_10_messages = data.tail(22)['Email Text'].tolist()'''
 
 # Define a function to clean and preprocess the text
 def clean_text(text):
