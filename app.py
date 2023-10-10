7a7b4cead7bdf633cb538b33d21bdec36d08462b
LAKSHMAN375 <125733859+LAKSHMAN375@users.noreply.github.com>
2023-10-10 19:58:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import string
 import re
@@ -10,21 +11,36 @@ import pickle
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Define the patterns
-patterns = [
-    r".*(bank|account|unusual activity|verify|details|unauthorized).*",
-    r".*(won|prize|lottery|claim|processing fee|offer is valid|free cash).*",
-    r".*(tech support|malware|infected|call|immediate assistance).*",
-    r".*(urgent|bank|unusual activity|secure|transactions).*",
-    r".*(tax notice|unclaimed tax refund|social security number|bank details).*",
-    r".*(subscription|canceled|payment issue|update payment details|disruption).*",
-    r".*(unlock|premium features|download|app|unlimited access).*"
-]
-
 # Define a function to clean and preprocess the text
 def clean_text(text):
-    # ... (rest of your code remains the same)
+    text = re.sub(r'http\S+', '', text)
+    text = word_tokenize(text)  # Tokenize
+    text = " ".join(text)  # Join tokens
+    text = [char for char in text if char not in string.punctuation]  # Remove punctuation
+    text = ''.join(text)  # Join the letters
+    text = [char for char in text if char not in re.findall(r"[0-9]", text)]  # Remove numbers
+    text = ''.join(text)  # Join the letters
+    text = [word.lower() for word in text.split() if word.lower() not in set(stopwords.words('english'))]  # Remove stopwords
+    text = ' '.join(text)  # Join the letters
     return text
+
+# Define a function to detect patterns in the text
+def detect_patterns(text):
+    patterns = [
+        r".*(bank|account|unusual activity|verify|details|unauthorized).*",
+        r".*(won|prize|lottery|claim|processing fee||offer is valid|| free cash).*",
+        r".*(tech support|malware|infected|call|immediate assistance).*",
+        r".*(urgent|bank|unusual activity|secure|transactions).*",
+        r".*(tax notice|unclaimed tax refund|social security number|bank details).*",
+        r".*(subscription|canceled|payment issue|update payment details|disruption).*",
+        r".*(unlock|premium features|download|app|unlimited access).*"
+    ]
+    
+    for pattern in patterns:
+        if re.match(pattern, text, re.IGNORECASE):
+            return True
+    
+    return False
 
 # Create a Streamlit app
 st.title('SMS Spam Classifier')
@@ -39,17 +55,13 @@ if st.button('Detect'):
         cleaned_sms = clean_text(input_sms)
 
         # Check for patterns
-        if any(re.match(pattern, cleaned_sms, re.IGNORECASE) for pattern in patterns):
-            # Vectorize the input
-            vector_input = tfidf.transform([cleaned_sms])
-
+        if detect_patterns(cleaned_sms):
             # Make a prediction
             result = model.predict(vector_input)
-
-            # Display the prediction
+             # Display the prediction
             if result == 1:
                 st.header("Spam")
             else:
                 st.header("Not Spam")
         else:
-            st.header("Enter Correct SMS Pattern")
+          st.header("Enter Correct Pattern")
