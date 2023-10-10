96749023019128a5c4e38c3b41771a696e597619
LAKSHMAN375 <125733859+LAKSHMAN375@users.noreply.github.com>
2023-10-10 22:19:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,24 +25,6 @@ def clean_text(text):
     text = ' '.join(text)  # Join the letters
     return text
 
-# Define a function to detect patterns in the text
-def detect_patterns(text):
-    patterns = [
-        r".*(bank|account|unusual activity|verify|details|unauthorized).*",
-        r".*(won|prize|lottery|claim|processing fee|offer is valid|free cash).*",
-        r".*(tech support|malware|infected|call|immediate assistance).*",
-        r".*(urgent|bank|unusual activity|secure|transactions).*",
-        r".*(tax notice|unclaimed tax refund|social security number|bank details).*",
-        r".*(subscription|canceled|payment issue|update payment details|disruption).*",
-        r".*(unlock|premium features|download|app|unlimited access).*"
-    ]
-    
-    for pattern in patterns:
-        if re.match(pattern, text, re.IGNORECASE):
-            return True
-    
-    return False
-
 # Create a Streamlit app
 st.title('SMS Spam Classifier')
 
@@ -55,17 +37,14 @@ if st.button('Detect'):
         # Preprocess the input SMS
         cleaned_sms = clean_text(input_sms)
 
-        # Check for patterns
-        if detect_patterns(cleaned_sms):
-            # Vectorize the input
-            vector_input = tfidf.transform([cleaned_sms])
+        # Vectorize the input
+        vector_input = tfidf.transform([cleaned_sms])
 
-            # Make a prediction
-            result = model.predict(vector_input)
-            # Display the prediction
-            if result == 1:
-                st.header("Spam")
-            else:
-                st.header("Not Spam")
+        # Make a prediction
+        result = model.predict(vector_input)
+
+        # Display the prediction
+        if result == 1:
+            st.header("Spam")
         else:
-            st.header("Enter Correct Pattern")
+            st.header("Not Spam")
